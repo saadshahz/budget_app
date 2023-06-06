e720e99ececb3faf46bc1958901f2206ab66d563
saadshahz <125588011+saadshahz@users.noreply.github.com>
2023-06-06 22:51:22 +0500

    addition

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const budget = document.getElementById("budget");
 const title = document.getElementById("title-expense");
 const product_cost =document.getElementById("cost");
 const expenses = document.getElementById("expeneses");
-const balance = document.getElementById("Balance");
+let balance = document.getElementById("Balance");
 const list = document.getElementById("list");
 const date_product = document.getElementById('date');
 let total_amount = document.getElementById("total-amount");
@@ -16,33 +16,36 @@ function setbudget(){
  }
 }
 
+let remain =0;
+
 function checkAmount(){
+   
 
       if(product_cost === ''|| title === ''|| date_product ===''){
          alert("Ener the value in the field")
       }
          else{
             expenses.innerText = product_cost.value
+                              //300-0
+                              //500-100=400-300=100
+            remain=(budget.value - product_cost.value);
+            balance.innerHTML = remain;
+         
+            const li = document.createElement("li");
+            li.innerHTML = title.value;
+            list.appendChild(li);
+            const span1 = document.createElement("span");
+            const span2 = document.createElement("span");
 
-    balance.innerText = budget.value -product_cost.value;
+            span1.innerHTML = date_product.value;
+            span2.innerHTML = product_cost.value;
 
-    const li = document.createElement("li");
-    li.innerHTML = title.value;
-    list.appendChild(li);
-    const span1 = document.createElement("span");
-    const span2 = document.createElement("span");
-
-    span1.innerHTML = date_product.value;
-    span2.innerHTML = product_cost.value;
-   const listTitle = document.createElement("h3")
-
-   listTitle.innerHTML = "TITLE"
-
-
-
-    li.appendChild(listTitle);
+ 
     li.appendChild(span1);
     li.appendChild(span2);
+    budget.value= remain;
+    title.value = "";
+        
     
          }
     
